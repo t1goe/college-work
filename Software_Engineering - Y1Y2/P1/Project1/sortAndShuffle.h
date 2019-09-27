3c9d838574e56c9b01a823956b46980c9daf0110
Tom Igoe <tigoe13@gmail.com>
2019-09-27 11:29:13 +0100

    Removed programs

--- Software_Engineering - Y1Y2/P1/Project1/sortAndShuffle.h
+++
@@ -1,8 +0,0 @@
-#define NAMELENGTH 80
-
-void sortArtists(char sortedArtists[][80] , int numOfArtists, int artistorder[]);
-
-void sortSongs(char songsOfAnArtist[][80], int numOfArtists);
-
-void shuffleSongs(char songsToBeShuffled[][80], int numOfSongs);
-
