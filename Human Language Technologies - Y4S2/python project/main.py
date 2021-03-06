822777978ab0f551ecbfcebc5f894dbc6fcbd620
Tom <44496697+t1goe@users.noreply.github.com>
2021-03-06 18:17:13 +0000

    started hlt 1

--- Human Language Technologies - Y4S2/python project/main.py
+++ Human Language Technologies - Y4S2/python project/main.py
@@ -5,6 +5,7 @@ sub = "todayilearned"  # the name of the subreddit (not including the 'r/')
 
 import praw
 from praw.models import MoreComments
+import nltk
 
 reddit = praw.Reddit(
     client_id=ci,
@@ -16,10 +17,22 @@ reddit = praw.Reddit(
 def remove_emojis(text):
     return text.encode('ascii', 'ignore').decode('ascii')
 
+
+def display_comments(comments, indent=" "):
+    for reply in comments:
+        if not hasattr(reply, 'body'):
+            return
+
+        tokens = nltk.word_tokenize(reply.body)
+        print(indent + " " + reply.body + "(length: " + str(len(tokens)) + ")")
+        display_comments(reply.replies, indent + "\t")
+
+
 with open(sub + ".txt", "w") as f:
     # on the following line you can change top to any of the previously mentioned ways of sorting
     # and the limit to however many posts you would like to extract (here we extract just 10).
-    for post in reddit.subreddit(sub).top(limit=10):
+    for post in reddit.subreddit(sub).top(limit=100):
+        display_comments(post.comments)
 
         # this line collects the post titles
         f.write(post.title + "\n")
