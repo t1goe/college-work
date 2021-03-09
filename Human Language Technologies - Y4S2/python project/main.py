e93046d51552fa05da7ef633c2f139df61e255a5
Tom <44496697+t1goe@users.noreply.github.com>
2021-03-09 15:25:37 +0000

    Added building subreddit corpuses (corpi?)

--- Human Language Technologies - Y4S2/python project/main.py
+++ Human Language Technologies - Y4S2/python project/main.py
@@ -1,30 +1,21 @@
-ci = "f2CDXIEJMw9amg"  # your client id
-cs = "dS2I4meWoXMgQ-AtZObOO8C5WM_smw"  # your client secret
-ua = "HLTUCDScript"  # your user agent name
-sub = "todayilearned"  # the name of the subreddit (not including the 'r/')
-
 import praw
 from praw.models import MoreComments
 import nltk
 import emoji
-
-# nltk.download('punkt')
-
-reddit = praw.Reddit(
-    client_id=ci,
-    client_secret=cs,
-    user_agent=ua
-)
+import os
 
 
+# Remove all emojis from string
 def remove_emojis(text):
     return text.encode('ascii', 'ignore').decode('ascii')
 
 
+# Convert all emojis in a string to :string_representation:
 def decode_emojis(text):
     return emoji.demojize(text)
 
 
+# Recursively display all comments
 def display_comments(comments, indent=" "):
     for reply in comments:
         if not hasattr(reply, 'body'):
@@ -36,18 +27,19 @@ def display_comments(comments, indent=" "):
         display_comments(reply.replies, indent + "\t")
 
 
+# Lexical complexity of a list of tokens
 def lexical_complexity(tokens):
     # tokens = nltk.word_tokenize(text)
     return len(set(tokens)) / len(tokens)
 
 
+# Average lex diversity of a set of comments
 def average_individual_lex_complexity(comments):
     diversity_scores = []
-    number_of_comments = 0
 
     for reply in comments:
         if not hasattr(reply, 'body'):
-            return diversity_scores, number_of_comments
+            return diversity_scores
 
         tokens = nltk.word_tokenize(reply.body)
         diversity_scores.append(lexical_complexity(tokens))
@@ -55,60 +47,85 @@ def average_individual_lex_complexity(comments):
         print(lexical_complexity(tokens))
         print()
 
-        number_of_comments = number_of_comments + 1
-
-        temp_diversity_list, temp_number_comments = average_individual_lex_complexity(reply.replies)
+        temp_diversity_list = average_individual_lex_complexity(reply.replies)
 
         diversity_scores.extend(temp_diversity_list)
-        number_of_comments = number_of_comments + temp_number_comments
 
-    return diversity_scores, number_of_comments
-
-def average_individual_lex_complexity(comments):
-    diversity_scores = []
-    number_of_comments = 0
-
-    for reply in comments:
-        if not hasattr(reply, 'body'):
-            return diversity_scores, number_of_comments
-
-        tokens = nltk.word_tokenize(decode_emojis(reply.body))
-        diversity_scores.append(lexical_complexity(tokens))
-        print(tokens)
-        print(lexical_complexity(tokens))
-        print()
-
-        number_of_comments = number_of_comments + 1
-
-        temp_diversity_list, temp_number_comments = average_individual_lex_complexity(reply.replies)
-
-        diversity_scores.extend(temp_diversity_list)
-        number_of_comments = number_of_comments + temp_number_comments
-
-    return diversity_scores, number_of_comments
+    return diversity_scores
 
 
-# on the following line you can change top to any of the previously mentioned ways of sorting
-# and the limit to however many posts you would like to extract (here we extract just 10).
-for post in reddit.subreddit(sub).top(limit=1):
-    d, n  = average_individual_lex_complexity(post.comments)
-    print(sum(d)/n)
+def build_subreddit_corpus(subreddit, reddit):
+    post_limit = 1
 
-with open(sub + ".txt", "w") as f:
+    if not os.path.exists(subreddit):
+        os.makedirs(subreddit)
+
+    ten = open(subreddit + "/gt10^1.txt", "w")
+    hundred = open(subreddit + "/gt10^2.txt", "w")
+    thousand = open(subreddit + "/gt10^3.txt", "w")
+    tenthousand = open(subreddit + "/gt10^4.txt", "w")
+    overall = open(subreddit + "/all.txt", "w")
+
     # on the following line you can change top to any of the previously mentioned ways of sorting
     # and the limit to however many posts you would like to extract (here we extract just 10).
-    for post in reddit.subreddit(sub).top(limit=1):
-        display_comments(post.comments)
-
-        # this line collects the post titles
-        f.write(post.title + "\n")
-
-        # this line collects the post content
-        f.write(post.selftext + "\n")
+    for post in reddit.subreddit(subreddit).top(limit=post_limit):
 
         # this section collects the comments
         for comment in post.comments:
             if isinstance(comment, MoreComments):
                 continue
 
-            f.write(decode_emojis(comment.body) + "\n")
+            if comment.score >= 10000:
+                tenthousand.write(decode_emojis(comment.body) + "\n")
+            elif comment.score >= 1000:
+                thousand.write(decode_emojis(comment.body) + "\n")
+            elif comment.score >= 100:
+                hundred.write(decode_emojis(comment.body) + "\n")
+            elif comment.score >= 10:
+                ten.write(decode_emojis(comment.body) + "\n")
+
+    ten.close()
+    hundred.close()
+    thousand.close()
+    tenthousand.close()
+    overall.close()
+
+ci = "f2CDXIEJMw9amg"  # your client id
+cs = "dS2I4meWoXMgQ-AtZObOO8C5WM_smw"  # your client secret
+ua = "HLTUCDScript"  # your user agent name
+sub = "todayilearned"  # the name of the subreddit (not including the 'r/')
+
+# nltk.download('punkt')
+
+reddit = praw.Reddit(
+    client_id=ci,
+    client_secret=cs,
+    user_agent=ua
+)
+
+build_subreddit_corpus(sub, reddit)
+
+# on the following line you can change top to any of the previously mentioned ways of sorting
+# and the limit to however many posts you would like to extract (here we extract just 10).
+# for post in reddit.subreddit(sub).top(limit=1):
+#     d = average_individual_lex_complexity(post.comments)
+#     print(sum(d) / len(d))
+
+# with open(sub + ".txt", "w") as f:
+#     # on the following line you can change top to any of the previously mentioned ways of sorting
+#     # and the limit to however many posts you would like to extract (here we extract just 10).
+#     for post in reddit.subreddit(sub).top(limit=1):
+#         display_comments(post.comments)
+#
+#         # this line collects the post titles
+#         f.write(post.title + "\n")
+#
+#         # this line collects the post content
+#         f.write(post.selftext + "\n")
+#
+#         # this section collects the comments
+#         for comment in post.comments:
+#             if isinstance(comment, MoreComments):
+#                 continue
+#
+#             f.write(decode_emojis(comment.body) + "\n")
